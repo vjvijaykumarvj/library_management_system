0f29ac6ef2a5adca4aa1315a2ab046b333447b04
vijay kumar <107472720+vjvijaykumarvj@users.noreply.github.com>
2026-05-08 11:14:17 +0530

    Update settings.py

--- Library_root/Library_project/settings.py
+++ Library_root/Library_project/settings.py
@@ -75,14 +75,22 @@ WSGI_APPLICATION = 'Library_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'vijay_db',
+#         'USER': 'root',
+#         'PASSWORD': 'Vj@13396m042',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vijay_db',
-        'USER': 'root',
-        'PASSWORD': 'Vj@13396m042',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
